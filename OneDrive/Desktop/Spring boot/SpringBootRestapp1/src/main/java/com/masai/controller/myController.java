f22e7b9321697c02b74436325a82e45caf1c4571
rawatshubham1645 <rawatshubham1645@gmail.com>
2023-01-15 01:39:25 +0530

    Spring boot rest app 1

--- OneDrive/Desktop/Spring boot/SpringBootRestapp1/src/main/java/com/masai/controller/myController.java
+++ OneDrive/Desktop/Spring boot/SpringBootRestapp1/src/main/java/com/masai/controller/myController.java
@@ -1,7 +1,14 @@
 package com.masai.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +17,8 @@ import com.masai.Model.Student;
 //@Controller
 @RestController  //(@Controller + @ResponseBody)
 // if you want to make whole class give raw data then we can use @Response instead of @Controller 
-// and noo need to write the @ResponseBody at every method
+// and no need to write the @ResponseBody at every method
+@RequestMapping("/studentapp")
 public class myController {
 	
 	
@@ -34,5 +42,18 @@ public class myController {
 	public Student myStudent(){
 		Student student = new Student(01,"Shubham","pune",99);
 		return student;
+	}
+	
+	@RequestMapping("/allStudents")
+	//@RequestMapping(value = "/allStudents", method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
+//	@GetMapping(value = "/allStudents", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping("/allStudents") //Shortcut
+	public List<Student> getAllStudent(){
+		List<Student> students = Arrays.asList(
+				new Student(01,"Shubham","pune",99),
+				new Student(02,"sourabh","Mumbai",99),
+				new Student(03,"kunal","Delhi",99),
+				new Student(04,"paras","Goa",99));
+		return students;
 	}
 }
